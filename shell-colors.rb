4df51242ea5dbe558136016a139a75a1a49cfb25
chrokh <christopher.okhravi@gmail.com>
2016-08-06 12:27:38 +0200

    Update shell-colors formula to latest version of keg

--- shell-colors.rb
+++ shell-colors.rb
@@ -1,8 +1,8 @@
 class ShellColors < Formula
   desc "Shell color scheme switcher for OSX"
   homepage "https://github.com/chrokh/shell-colors"
-  url "https://github.com/chrokh/shell-colors/archive/0.0.2.tar.gz"
-  sha256 "2d8d4202090275e02f4c4ad5e05b58b7bf72ca6a216026afb5579d9a6e8d1abf"
+  url "https://github.com/chrokh/shell-colors/archive/0.0.3.tar.gz"
+  sha256 "0b400d62403b920988144224269e9a9cf9484623107138a91a2aff4262347c4f"
 
   def install
     bin.install "theme"

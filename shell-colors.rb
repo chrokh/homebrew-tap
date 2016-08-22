527f379bd7c30d7cb9de937ca0464f26dadd66aa
chrokh <christopher.okhravi@gmail.com>
2016-08-22 10:49:24 +0200

    Update shell-colors version

--- shell-colors.rb
+++ shell-colors.rb
@@ -1,8 +1,8 @@
 class ShellColors < Formula
   desc "Shell color scheme switcher for OSX"
   homepage "https://github.com/chrokh/shell-colors"
-  url "https://github.com/chrokh/shell-colors/archive/0.0.3.tar.gz"
-  sha256 "0b400d62403b920988144224269e9a9cf9484623107138a91a2aff4262347c4f"
+  url "https://github.com/chrokh/shell-colors/archive/0.0.4.tar.gz"
+  sha256 "80d7f1da8c8803969b53680d540574c6c06bbaf9b778505a497ecdcdea0fad55"
 
   def install
     bin.install "theme"

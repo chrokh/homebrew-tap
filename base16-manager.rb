d9882689290cea26257489c3ec277962a34c9d9a
chrokh <christopher.okhravi@gmail.com>
2018-01-26 14:55:47 +0100

    Update base16-manager version.

--- base16-manager.rb
+++ base16-manager.rb
@@ -1,8 +1,9 @@
 class Base16Manager < Formula
   desc "A command line tool to install base16 templates and set themes globally."
   homepage "https://github.com/AuditeMarlow/base16-manager"
-  url "https://github.com/AuditeMarlow/base16-manager/archive/1.0.0.tar.gz"
-  sha256 "7ef20b904733e6a619c9d951843ce122817b78cf922320378d58720e8dfe0e21"
+  url "https://github.com/AuditeMarlow/base16-manager/archive/0bb194266ed1c32e9b36a1ad6a483a005caeb12c.tar.gz"
+  sha256 "6d90262f302958b807270a013a5aee8a709df91f5ea7fcfa7dc8d7696a80b5f1"
+  version "1.0.1"
 
   def install
     bin.install "base16-manager"

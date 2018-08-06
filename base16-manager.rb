eae2061acb123985e9527600c123684ed5683bdc
chrokh <christopher.okhravi@gmail.com>
2018-08-06 15:36:36 +0200

    Fix incorrect checksum for base16-manager 1.2.1.

--- base16-manager.rb
+++ base16-manager.rb
@@ -2,7 +2,7 @@ class Base16Manager < Formula
   desc "A command line tool to install base16 templates and set themes globally."
   homepage "https://github.com/AuditeMarlow/base16-manager"
   url "https://github.com/AuditeMarlow/base16-manager/archive/1.2.1.tar.gz"
-  sha256 "95ecee27bbdc67e99361e9efb8acd922d46ef018614c48ced2c8306e232a4bbd"
+  sha256 "491e7616f9a69ca82d953c377b94b8dacc5478178827a2d9ca31f624b0a189bd"
   version "1.2.1"
 
   def install
